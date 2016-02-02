4664d31f4fe008d2d148ef07cb60436d6d8f7f9d
ReemAlattas <ralattas@live.com>
2016-02-02 14:26:58 -0500

    First Commit

--- prime/ViewController.swift
+++ prime/ViewController.swift
@@ -19,6 +19,55 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var num: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func primeButton(sender: AnyObject) {
+        
+        var numInt = Int(num.text!)
+        //print(numInt)
+        
+        if numInt != nil {
+            
+            var isPrime = true
+            
+            if numInt == 1 {
+                
+                isPrime = false
+                
+            }
+            
+            if numInt != 1 && numInt != 2 {
+                
+                for var i = 2; i < numInt; i++ {
+                    
+                    if numInt! % i == 0 {
+                        
+                        isPrime = false
+                        
+                    }
+                }
+            }
+            
+            if isPrime {
+            
+                result.text = "Prime!"
+                
+            } else {
+                
+                result.text = "Not Prime!"
+                
+            }
+            
+        }
+            
+        else {
+            result.text = "Please enter a number in the box"
+        }
+        
+    }
 
 
 }
